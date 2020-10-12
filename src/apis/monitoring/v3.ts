f78a5b40bb0cce43f5d568e25a6c81da216fa2c8
yoshi-automation <yoshi-automation@google.com>
2020-10-12 05:22:57 -0700

    feat(monitoring): update the API
    #### monitoring:v3
    The following keys were added:
    - schemas.Condition.properties.conditionMonitoringQueryLanguage.$ref
    - schemas.Condition.properties.conditionMonitoringQueryLanguage.description
    - schemas.MonitoringQueryLanguageCondition.description
    - schemas.MonitoringQueryLanguageCondition.id
    - schemas.MonitoringQueryLanguageCondition.properties.duration.description
    - schemas.MonitoringQueryLanguageCondition.properties.duration.format
    - schemas.MonitoringQueryLanguageCondition.properties.duration.type
    - schemas.MonitoringQueryLanguageCondition.properties.query.description
    - schemas.MonitoringQueryLanguageCondition.properties.query.type
    - schemas.MonitoringQueryLanguageCondition.properties.trigger.$ref
    - schemas.MonitoringQueryLanguageCondition.properties.trigger.description
    - schemas.MonitoringQueryLanguageCondition.type

--- src/apis/monitoring/v3.ts
+++ src/apis/monitoring/v3.ts
@@ -392,6 +392,10 @@ export namespace monitoring_v3 {
      */
     conditionAbsent?: Schema$MetricAbsence;
     /**
+     * A condition that uses the Monitoring Query Language to define alerts.
+     */
+    conditionMonitoringQueryLanguage?: Schema$MonitoringQueryLanguageCondition;
+    /**
      * A condition that compares a time series against a threshold.
      */
     conditionThreshold?: Schema$MetricThreshold;
@@ -1245,6 +1249,23 @@ export namespace monitoring_v3 {
      * Output only. A map of user-defined metadata labels.
      */
     userLabels?: {[key: string]: string} | null;
+  }
+  /**
+   * A condition type that allows alert policies to be defined using Monitoring Query Language.
+   */
+  export interface Schema$MonitoringQueryLanguageCondition {
+    /**
+     * The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported. If an invalid value is given, an error will be returned. When choosing a duration, it is useful to keep in mind the frequency of the underlying time series data (which may also be affected by any alignments specified in the aggregations field); a good duration is long enough so that a single outlier does not generate spurious alerts, but short enough that unhealthy states are detected and alerted on quickly.
+     */
+    duration?: string | null;
+    /**
+     * Monitoring Query Language query that outputs a boolean stream.
+     */
+    query?: string | null;
+    /**
+     * The number/percent of time series for which the comparison must hold in order for the condition to trigger. If unspecified, then the condition will trigger if the comparison is true for any of the time series that have been identified by filter and aggregations, or by the ratio, if denominator_filter and denominator_aggregations are specified.
+     */
+    trigger?: Schema$Trigger;
   }
   /**
    * Describes a change made to a configuration.
